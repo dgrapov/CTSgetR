a9e687e48717cdf90bd9a62b7adedfe2a372f8e2
Dmitry Grapov <dgrapov@gmail.com>
2013-05-03 23:53:16 -0700

    updating to production server

--- R/zCTS.R
+++ R/zCTS.R
@@ -1,4 +1,4 @@
-CTS.getR<-function(id,from,to,parallel=FALSE,server="http://uranus.fiehnlab.ucdavis.edu:8080/cts-2.0-beta-2/service/convert"){ 
+CTS.getR<-function(id,from,to,parallel=FALSE,server="http://cts.fiehnlab.ucdavis.edu/service/convert"){ 
 		# id = vector of identifiers
 		#to/from =	"Chemical Name","InChIKey","InChI Code","PubChem CID","Pubchem SID","ChemDB","ZINC","Southern Research Institute","Specs","MolPort","ASINEX","ChemBank","MLSMR","Emory University Molecular Libraries Screening Center","ChemSpider","DiscoveryGate","Ambinter","Vitas-M Laboratory","ChemBlock"
 		#result is a 3 column data frame of from, to and translated values
@@ -43,39 +43,10 @@ CTS.getR<-function(id,from,to,parallel=FALSE,server="http://uranus.fiehnlab.ucda
 		#parse  into a triple
 		tmp<-do.call("cbind",out)
 		tmp<-cbind(from=id,to=to,result=as.character(unlist(tmp[4,])))
-		colnames(tmp)<-c(paste("from",from,sep=" :"),"to:","RESULT")
-		return(tmp)
+		colnames(tmp)<-c(paste("from",from,sep=" :"),"to:",to)
+		return(tmp[,3,drop=FALSE])
 	}
 	
-# CTS.translate<-function(server,from,to,id,number=NULL){ # ussues with loss of connection, seems to be due to RCurl?
-		# #require("RCurl")
-		# # results are returned as JSON encoded strings
-		# url<-paste(server,from,to,id,sep="/")
-		# url<-gsub("\\ ","%20",url) # fix spaces 
-		
-		# if(!is.null(number)) # control number of asyncronous calls getURL (too many can lead to connection problems)
-			# {
-				# if(number>length(id)){number<-length(id)}
-				# end.id<-c(1:length(id))[c(1:length(id))%%number==0]
-				# start.id<-end.id-number+1
-				# extra<-length(id)-max(end.id)# for the ends
-				# start.id<-c(start.id,(max(end.id)+1))
-				# end.id<-c(end.id,max(end.id+extra))
-				
-				# content<-as.character(unlist(sapply(1:length(end.id), function(i,start=start.id, val=url, pb = txtProgressBar(min = 0, max = length(end.id), style = 3))
-					# {
-						# setTxtProgressBar(pb, i)
-						# tmp<-val[start[i]:end.id[i]]
-						# as.character(unlist(getURL(tmp,ssl.verifypeer=FALSE)))
-						# closeAllConnections() # trying to avoid loss of connection... may want to create a dedicated connections and keep calling it
-					# })))
-			
-			# } else {
-				# content<-as.character(unlist(getURL(url,ssl.verifypeer=FALSE)))
-			# }
-		
-			# return(content)
-# }
 
 CTS.translate<-function(server,from,to,id,parallel=FALSE){ #arguably parallel, seems more connection stable than asynchronous
 		#require("RCurl")
@@ -84,6 +55,7 @@ CTS.translate<-function(server,from,to,id,parallel=FALSE){ #arguably parallel, s
 		url<-gsub("\\ ","%20",url) # fix spaces 
 		
 		if(parallel==TRUE){ # not clear if actuall improves speed, why RCurls was tried first
+				library(snow);library(doSNOW);library(foreach)
 				cl.tmp = makeCluster(rep("localhost",Sys.getenv('NUMBER_OF_PROCESSORS')), type="SOCK")  # windows specific
 				registerDoSNOW(cl.tmp) 
 				#do work
@@ -94,7 +66,7 @@ CTS.translate<-function(server,from,to,id,parallel=FALSE){ #arguably parallel, s
 			content<-as.character(unlist(sapply(1:length(id), function(i, pb = txtProgressBar(min = 0, max = length(id), style = 3))
 				{
 					setTxtProgressBar(pb, i)
-					readLines(url[i])
+					tryCatch(readLines(url[i]),error=function(e){"error"})
 				})))
 		}
 			return(content)
@@ -103,7 +75,7 @@ CTS.translate<-function(server,from,to,id,parallel=FALSE){ #arguably parallel, s
 # get possible translations from CTS
 CTS.options<-function(){
 	options(warn=-1)	
-	url<-readLines("http://cts.fiehnlab.ucdavis.edu/service/convert/fromValues")
+	url<-readLines("http://cts.fiehnlab.ucdavis.edu/service/convert/toValues")
 	tmp<-unlist(strsplit(url[1],""))
 	obj<-tmp[-c(1:2,(length(tmp)-2):length(tmp))]
 	unlist(strsplit(paste(obj[!obj=='\"'],collapse=""),","))
