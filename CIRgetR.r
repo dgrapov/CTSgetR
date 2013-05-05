bafe030871e5b0e09ecf13817ee3a9e3aabea211
Dmitry Grapov <dgrapov@gmail.com>
2013-05-05 09:51:43 -0700

    removing . from fxn name

--- CIRgetR.r
+++ CIRgetR.r
@@ -1,5 +1,3 @@
-# R interface to  Chemical Identifier Resolver (CIR) by the CADD Group at the NCI/NIH
-# see py version: https://github.com/mcs07/CIRpy/blob/master/README.md
 CIRgetR<-function(id,to=c("pubchem_sid"),return.all=TRUE,progress=TRUE){
 		#id needs to be  one of the folowing types of structural ids "inchi","inchiKey" or "smiles"
 		#to can be:	smiles, names, iupac_name, cas, inchi, 
@@ -46,20 +44,5 @@ CIRgetR<-function(id,to=c("pubchem_sid"),return.all=TRUE,progress=TRUE){
 		
 		results<-matrix(out,ncol=1)
 		colnames(results)<-to
-		return(results)
+		return(data.frame(results))
 		}
-
-#test
-id<- c("C[N+](C)(C)[O-]", "CC(=O)Oc1ccccc1C(=O)O")		
-opts<-c("smiles", "names", "iupac_name", "cas", "inchi", "stdinchi", "inchikey", "stdinchikey",
-		"ficts", "ficus", "uuuuu", "image", "file", "mw", "monoisotopic_mass","chemspider_id",
-		"pubchem_sid", "chemnavigator_sid", "formula", "chemnavigator_sid")		
-		
-translations<-sapply(1:length(opts), function(i)
-	{
-		CIRgetR(id=id,to=opts[i],return.all=FALSE)
-	})
-colnames(translations)<-opts	
-
-#image test
-CIRgetR(id,to="image")
