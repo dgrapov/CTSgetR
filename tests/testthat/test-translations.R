25d74d8a270b1612b40b47f0bb751ef9862276a2
dgrapov <dgrapov@gmail.com>
2020-07-09 18:51:37 -0700

    fix tests

--- tests/testthat/test-translations.R
+++ tests/testthat/test-translations.R
@@ -1,6 +1,10 @@
 #skip_on_cran(tests
 context("CTS translation")
 
+#test agains local db or set to NULL to test API
+db_name<-'../../inst/ctsgetr.sqlite'
+
+print(getwd())
 #in
 id <-
   c(
@@ -28,76 +32,23 @@ from <- "KEGG"
 to <- "PubChem CID"
 
 #expected result
-res <-
-  structure(
-    list(
-      id = structure(
-        1:19,
-        .Label = c(
-          "C15973",
-          "C00026",
-          "C05381",
-          "C15972",
-          "C00091",
-          "C00042",
-          "C05379",
-          "C00311",
-          "C00036",
-          "C00024",
-          "C00149",
-          "C00417",
-          "C00158",
-          "C00022",
-          "C05125",
-          "C16254",
-          "C00122",
-          "C16255",
-          "C00074"
-        ),
-        class = "factor"
-      ),
-      from = structure(
-        c(1L,
-          1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
-          1L, 1L),
-        .Label = "KEGG",
-        class = "factor"
-      ),
-      to = structure(
-        c(1L,
-          1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
-          1L, 1L),
-        .Label = "PubChem CID",
-        class = "factor"
-      ),
-      key = c(
-        NA,
-        "51",
-        "440649",
-        NA,
-        "92133",
-        "1110",
-        "972",
-        "1198",
-        "970",
-        "444493",
-        "222656",
-        "643757",
-        "19782904",
-        "1060",
-        "440568",
-        NA,
-        "21883788",
-        NA,
-        "1005"
-      )
-    ),
-    row.names = c(NA, 19L),
-    class = "data.frame"
-  )
+res<-structure(list(id = c("C15973", "C00026", "C05381", "C15972", 
+                           "C00091", "C00042", "C05379", "C00311", "C00036", "C00024", "C00149", 
+                           "C00417", "C00158", "C00022", "C05125", "C16254", "C00122", "C16255", 
+                           "C00074"), from = c("KEGG", "KEGG", "KEGG", "KEGG", "KEGG", "KEGG", 
+                                               "KEGG", "KEGG", "KEGG", "KEGG", "KEGG", "KEGG", "KEGG", "KEGG", 
+                                               "KEGG", "KEGG", "KEGG", "KEGG", "KEGG"), to = c("PubChem CID", 
+                                                                                               "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", 
+                                                                                               "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", 
+                                                                                               "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", "PubChem CID", 
+                                                                                               "PubChem CID", "PubChem CID", "PubChem CID"), key = c(NA, "51", 
+                                                                                                                                                     "440649", NA, "92133", "1110", "972", "1198", "970", "444493", 
+                                                                                                                                                     "222656", "643757", "19782904", "1060", "440568", NA, "21883788", 
+                                                                                                                                                     NA, "1005")), row.names = c(NA, -19L), class = "data.frame")
 
 
 test_that("one to one translation", {
   skip_on_cran()
-  expect_equal(CTSgetR(id, from, to,dbname='inst/ctsgetr.sqlite'), res)
+  expect_equivalent(CTSgetR(id, from, to,db_name), res)
+  # expect_equal(getwd(), 'foo')
 })
